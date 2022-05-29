5eb5195d11ecd492a7cf1ae45ef066ad79c26ccb
vladpanishev <vladislav.panishev1989@gmail.com>
2022-05-29 13:02:11 +0300

    Улучшил мобильную версию сайта

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,34 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+// POPUP
+const btns = document.querySelectorAll('.btn__portfolio');
+const modalOverlay = document.querySelector('.popup__body');
+const modals = document.querySelectorAll('.popup__content');
+
+btns.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    let path = e.currentTarget.getAttribute('data-path');
+
+    modals.forEach((el) => {
+      el.classList.remove('popup__content--visible');
+    });
+
+    document
+      .querySelector(`[data-target="${path}"]`)
+      .classList.add('popup__content--visible');
+    modalOverlay.classList.add('popup__body--visible');
+  });
+});
+
+modalOverlay.addEventListener('click', (e) => {
+  console.log(e.target);
+
+  if (e.target == modalOverlay) {
+    modalOverlay.classList.remove('popup__body--visible');
+    modals.forEach((el) => {
+      el.classList.remove('popup__content--visible');
+    });
+  }
+});
